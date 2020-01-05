ec4e57405e51413314669defd80b53561fda213a
sainath2597 <sainath2597@gmail.com>
2020-01-05 11:37:13 +0000

    first commit

---
+++ Cal.py
@@ -0,0 +1,10 @@
+class Calculator:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+    def add(self):
+        c = self.a+self.b
+        return c
+    def sub(self):
+        c = self.b-self.a
+        return c
---
+++ test_cal.py
@@ -0,0 +1,10 @@
+import unittest
+from Cal import Calculator
+class testCal(unittest.TestCase):
+    def test_oper(self):
+        ca=Calculator(2,3)
+        self.assertEqual(ca.add(),5, msg='Add 2 numbers')
+        self.assertEqual(ca.sub(),1, msg='Sub 2 numbers')
+
+if __name__ == '__main__':
+    unittest.main()
